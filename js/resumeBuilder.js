7c077c7ba942f269a40c27c3f8ff582e47a78ebc
Tiana Tucker <tianatucker@gmail.com>
2015-11-16 23:09:35 -0500

    Lesson 2.3 Javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var awesomeThoughts = "I am Tiana and I am AWESOME!";
+
+var funThoughts =
+	awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
+
+
